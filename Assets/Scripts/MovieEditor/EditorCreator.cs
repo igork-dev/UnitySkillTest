0c63f6289535ef970d0a19a5db6fde28035c1ec4
Игорь <1g0rrr.my@gmail.com>
2014-11-10 23:47:46 +0200

    init

---
+++ Assets/Scripts/MovieEditor/EditorCreator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EditorCreator : MonoBehaviour {
+	
+	public Vertex InstantiateVertex( Vector3 position ) {
+		GameObject vertexGO = Instantiate( Resources.Load("Vertex"), position, Quaternion.identity ) as GameObject;
+		return vertexGO.GetComponent<Vertex>();
+	}
+	
+	public Line InstantiateLine( Vertex vertexA, Vertex vertexB ) {
+		
+		GameObject lineGO = Instantiate( Resources.Load("Line"), Vector3.zero, Quaternion.identity ) as GameObject;
+		lineGO.renderer.enabled = false;
+		Line line = lineGO.GetComponent<Line>();
+		
+		line.Initialize( vertexA, vertexB );
+		lineGO.renderer.enabled = true;
+		
+		return line;
+	}
+}
